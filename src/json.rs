ddf4ac99ff787311a639045da5f9efc010fc2c67
NotAShelf <raf@notashelf.dev>
2025-07-24 19:38:48 +0300

    json: allow unused code
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I6a6a69643e965fd8c3d9fc5e72ff48b14d57d8d4

--- src/json.rs
+++ src/json.rs
@@ -28,6 +28,7 @@ impl Display for Error {
 
 impl std::error::Error for Error {}
 
+#[allow(unused)]
 impl<'v> Value<'v> {
     #[must_use]
     pub const fn new(value: &'v serde_json::Value) -> Self {
